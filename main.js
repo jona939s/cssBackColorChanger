b4a669864f88fb89dbd8d3b1d87adaf9b1af44f1
JJoe <48448023+jona939s@users.noreply.github.com>
2022-04-30 00:51:21 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,17 +1,23 @@
 javascript:(function(){
-    let choiceBox = confirm("Use hex or string (ok for string, cancle for RGB)");
-    if (choiceBox){
-        let colorString = prompt("Write the name of a color", "black");
-        if (colorString == null || colorString == "") return;
-        else{
-            colorString.toLowerCase();
-            document.body.style.background = colorString.toString();
-            return;
-        }
-    } else {
-        var R = parseInt(prompt("Enter Red Value"));
-        var G = parseInt(prompt("Enter Green Value"));
-        var B = parseInt(prompt("Enter Blue Value"));
-        document.body.style.background = 'rgb(' + R + ',' + G + ',' + B + ')';
-    }
+let choiceBox = confirm("Press ok for a custom color and cancle to invert colors");
+if (choiceBox){
+let colorString = prompt("Write the name of a color", "black");
+if (colorString == null || colorString == "") return;
+else{
+colorString.toLowerCase();
+document.body.style.background = colorString.toString();
+return;
+}
+} else {}
+let invertChoice = confirm("Invert the colors?");
+if (invertChoice){
+document.getElementsByTagName("html")[0].style.filter = "invert()";
+if (typeof elementos === 'undefined') {let elementos = []}
+elementos = document.querySelectorAll(" a, img, video");
+for(let i = 0; i < elementos.length; i++){
+if ((elementos[i].nodeName == "A" && (elementos[i].style.background != "" || elementos[i].style.backgroundImage != "")) || elementos[i].nodeName != "A"){
+elementos[i].style.filter = "invert()";
+}
+}
+}
 })();
